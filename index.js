fbf84d088cf89fc965018665552867a166a6c8e2
Jav-23 <javierdelgado1897@gmail.com>
2025-05-02 18:51:46 -0500

    my-proyect

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn =document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click', function () {
+  alert('Bueno hoy cumplimos otro mes amandonos')
+});
+
+const noBtn =document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',function () {
+    alert('Entonces toque el Si')  
+});
