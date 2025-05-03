b283eabf37fc5d76e209cf100a1bcbf841ad4463
Jav-23 <javierdelgado1897@gmail.com>
2025-05-02 19:02:11 -0500

    my-proyect

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const yesBtn =document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click', function () {
   alert('Bueno hoy cumplimos otro mes amandonos')
+  ('Feliz mes mi amor &#128149;')
 });
 
 const noBtn =document.querySelector('#noBtn');
